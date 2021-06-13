3f9d70d52ec109b3130d198e5c3f50cd1815f752
simidagogogo <zhngd5191@outlook.com>
2021-06-13 15:50:40 +0800

    feature: init

---
+++ src/main/scala/PragmaticScala/others/StringExample.scala
@@ -0,0 +1,17 @@
+package PragmaticScala.others
+
+/**
+ * @author zhangda
+ * @date 2021/4/5 16:03
+ * @note
+ */
+object StringExample {
+
+  def main(args: Array[String]): Unit = {
+    val str = "lianmeng"
+
+    println(str.capitalize)
+    println(str.lines.toString())
+  }
+
+}
